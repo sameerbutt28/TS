3c73b13343284a248ef70b66e27d5076195395c1
sameerbutt28 <sameerofficial929@gmail.com>
2024-02-15 00:15:58 +0500

    REFACTORING

--- index.js
+++
@@ -1,5 +0,0 @@
-var age = 20;
-// age='s'; 
-// here as we know  that the age is assigned with a number so we cant give it a string.
-//helps more in debugging
-//with the help of typescript we can cach alot of our mistakes at compile time   
--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ let age:number = 20; //here the tpe annotton is only for the typescript not for 
 // age='s'; 
 // here as we know  that the age is assigned with a number so we cant give it a string.
 //helps more in debugging
-//with the help of typescript we can cach alot of our mistakes at compile time   
+//with the help of typescript we can cach alot of our mistakes at compile time  
+if(age < 50 )
+{
+    age +=10;
+} 
