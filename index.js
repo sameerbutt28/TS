87993e7f314b6ffeb8480ea016ba10cf3c636951
sameerbutt28 <sameerofficial929@gmail.com>
2024-02-14 19:56:14 +0500

    ADDING COMMENTS ABOUT ANOTATION

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('hello sameer ');
+var age = 20;
+// age='s'; 
+// here as we know  that the age is assigned with a number so we cant give it a string.
+//helps more in debugging
+//with the help of typescript we can cach alot of our mistakes at compile time   
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-let age:number = 20;
+let age:number = 20; //here the tpe annotton is only for the typescript not for the javascript.
 // age='s'; 
 // here as we know  that the age is assigned with a number so we cant give it a string.
 //helps more in debugging
